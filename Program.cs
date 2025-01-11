94ef1fa39411b916e21433631707d8de202cd2d6
Badr Ahmed Abdel_hady <123932289+BadrAhmed140@users.noreply.github.com>
2025-01-11 12:24:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using opp2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+       
+       
+
+
+        HireDate hireDate1 = new HireDate(5, 1, 2020);
+        HireDate hireDate2 = new HireDate(10, 4, 2021);
+        HireDate hireDate3 = new HireDate(15, 8, 2022);
+
+        Q1[] empArray = new Q1[3];
+
+            
+        empArray[0] = new Q1(1, "BADR", SecurityLevel.DBA, 85000, hireDate1, "M");
+        empArray[1] = new Q1(2, "bico", SecurityLevel.Guest, 40000, hireDate2, "F");
+        empArray[2] = new Q1(3, "tarek", SecurityLevel.Developer , 120000, hireDate3, "M");
+        foreach (Q1 emp in empArray) { 
+        Console.WriteLine(emp.ToString());
+            Console.WriteLine("----------------------------");
+        }
+    }
+}
---
+++ Q1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opp2
+{
+
+
+
+    public enum SecurityLevel
+    {
+        Guest,
+        Developer,
+        Secretary,
+        DBA
+    }
+    internal class HireDate
+    {
+        public int Day { get;  set; }
+        public int Month { get;  set; }
+        public int Year { get;  set; }
+
+
+
+        public HireDate(int day, int month, int year)
+        {
+
+
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+
+
+        public override string ToString() { 
+        return $"{Year:D4}-{Month:D2}-{Day:D2}";
+        }
+    }
+
+
+    internal class Q1
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public SecurityLevel SecurityLevel { get; set; }
+        public double Salary { get; set; }
+        public HireDate HireDate { get; set; }
+        private string gender;
+        public string Gender
+        {
+            get  { return gender; }
+             set
+            {
+                if (value == "M" || value == "F"|| value == "m" || value == "f")
+                {
+                    gender = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Gender must be 'M' (Male) or 'F' (Female).");
+                }
+            }
+        }
+
+        public Q1 (int id,string name , SecurityLevel secsecurityLevel, double salary ,HireDate hireDate,string gender)
+        {
+            Id = id;    
+            Name = name;
+            SecurityLevel = secsecurityLevel;
+            Salary = salary;
+            HireDate = hireDate;
+            Gender = gender;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Employee ID: {Id}\n" +
+              $"Name: {Name}\n" +
+              $"Security Level: {SecurityLevel}\n" +
+              $"Salary: {Salary}\n" +
+              $"Hire Date: {HireDate:yyyy-MM-dd}\n" +
+              $"Gender: {Gender}";
+        }
+
+    }
+}
